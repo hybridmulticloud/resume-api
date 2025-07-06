956ccae7d0437ddaa821de356537c2567c0275d3
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 03:03:26 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,34 +1,29 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "lambda_function_name" {
   description = "Lambda function name"
+  type        = string
   default     = "UpdateVisitorCount"
 }
 
 variable "dynamodb_table_name" {
   description = "DynamoDB table name"
+  type        = string
   default     = "VisitorCount"
 }
 
 variable "lambda_runtime" {
-  description = "Lambda function runtime"
-  default     = "python3.11"
-}
-variable "lambda_s3_bucket" {
-  description = "Name for the S3 bucket to store Lambda ZIP"
+  description = "Runtime environment for the Lambda function"
   type        = string
-  default     = "my-lambda-zip-bucket-${random_id.suffix.hex}"
+  default     = "python3.12"
 }
 
 variable "lambda_s3_key" {
-  description = "Path (key) in S3 bucket for Lambda ZIP"
+  description = "The key (filename) for the Lambda zip in S3"
   type        = string
   default     = "lambda_function.zip"
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4
 }
