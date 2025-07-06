76ce42670a478c15f81af6f071d68f40afa6df71
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 01:31:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,59 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# DynamoDB Table
+resource "aws_dynamodb_table" "visitors" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy Attachment
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "visitor_counter" {
+  function_name = var.lambda_function_name
+  handler       = "lambda_function.lambda_handler"
+  runtime       = var.lambda_runtime
+  role          = aws_iam_role.lambda_exec_role.arn
+  filename      = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+  timeout       = 10
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.visitors.name
+    }
+  }
+}
+
+# API Gateway v2 (HTTP API)
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "visitor-api"
