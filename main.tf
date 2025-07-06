3469691205c33ddb5aa40e9a270b8f4499cae61a
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 03:02:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+# Generate random suffix for S3 bucket
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
 # DynamoDB Table
@@ -31,8 +40,8 @@ resource "aws_iam_role" "lambda_exec_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -47,24 +56,26 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-
-# Lambda Function from S3
+# S3 bucket for Lambda ZIP
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = var.lambda_s3_bucket
+  bucket        = "lambda-zip-bucket-${random_id.suffix.hex}"
   force_destroy = true
 
   tags = {
-    Name = "Lambda Deployment Bucket"
+    Name        = "Lambda Deployment Bucket"
     Environment = "Terraform"
   }
 }
 
+# Upload Lambda ZIP to S3
 resource "aws_s3_object" "lambda_zip" {
   bucket = aws_s3_bucket.lambda_bucket.id
   key    = var.lambda_s3_key
   source = "${path.module}/lambda_function.zip"
   etag   = filemd5("${path.module}/lambda_function.zip")
 }
+
+# Lambda Function
 resource "aws_lambda_function" "visitor_counter" {
   function_name = var.lambda_function_name
   handler       = "lambda_function.lambda_handler"
@@ -72,7 +83,6 @@ resource "aws_lambda_function" "visitor_counter" {
   role          = aws_iam_role.lambda_exec_role.arn
   s3_bucket     = aws_s3_bucket.lambda_bucket.id
   s3_key        = aws_s3_object.lambda_zip.key
-  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
   timeout       = 10
 
   environment {
@@ -80,6 +90,8 @@ resource "aws_lambda_function" "visitor_counter" {
       TABLE_NAME = aws_dynamodb_table.visitors.name
     }
   }
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_logs]
 }
 
 # API Gateway v2 (HTTP API)
@@ -90,10 +102,10 @@ resource "aws_apigatewayv2_api" "http_api" {
 
 # API Integration
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.visitor_counter.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.visitor_counter.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
@@ -111,7 +123,7 @@ resource "aws_apigatewayv2_stage" "lambda_stage" {
   auto_deploy = true
 }
 
-# Permission for API Gateway to invoke Lambda
+# Lambda Permission for API Gateway
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
