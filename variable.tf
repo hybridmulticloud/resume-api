d185a7aec0f1901e2e2c895e4c451bc69da4aa4a
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 02:52:01 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -17,3 +17,12 @@ variable "lambda_runtime" {
   description = "Lambda function runtime"
   default     = "python3.11"
 }
+variable "lambda_s3_bucket" {
+  description = "S3 bucket containing the Lambda function ZIP"
+  type        = string
+}
+
+variable "lambda_s3_key" {
+  description = "S3 key (path) to the Lambda function ZIP file"
+  type        = string
+}
