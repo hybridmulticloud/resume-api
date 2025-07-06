c710419f746d1922516ae62fdcf8717148a4c421
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 02:45:40 +0100

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  description = "Lambda function name"
+  default     = "UpdateVisitorCount"
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name"
+  default     = "VisitorCount"
+}
+
+variable "lambda_runtime" {
+  description = "Lambda function runtime"
+  default     = "python3.11"
+}
