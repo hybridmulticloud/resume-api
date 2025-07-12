59dc5b5ed06c6f31677d1af1b88d9d4aa8a671fe
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-12 21:30:16 +0100

    Update lambda_function.py
    logging

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,39 @@
 import json
 import boto3
+import logging
+
+# Setup logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Connect to DynamoDB
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('VisitorCount')
 
 def lambda_handler(event, context):
-    response = table.update_item(
-        Key={'id': 'count'},
-        UpdateExpression='SET visits = visits + :inc',
-        ExpressionAttributeValues={':inc': 1},
-        ReturnValues='UPDATED_NEW'
-    )
+    logger.info("Lambda invoked with event: %s", json.dumps(event))
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps({
-            'visits': int(response['Attributes']['visits'])
-        })
-    }
+    try:
+        response = table.update_item(
+            Key={'id': 'count'},
+            UpdateExpression='SET visits = visits + :inc',
+            ExpressionAttributeValues={':inc': 1},
+            ReturnValues='UPDATED_NEW'
+        )
+        visits = int(response['Attributes']['visits'])
+        logger.info("Visit count updated successfully: %d", visits)
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'visits': visits})
+        }
+
+    except Exception as e:
+        logger.error("Error updating visit count: %s", str(e), exc_info=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal Server Error'})
+        }
