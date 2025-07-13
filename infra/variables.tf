ff56e9da2f7f87735ddc1d50c81d609cc61f5c71
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-13 02:12:28 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,29 @@
 variable "project_name" {
-  default = "resume-api"
+  description = "Base name for all resources"
+  type        = string
+  default     = "resume-api"
 }
 
 variable "lambda_function_name" {
-  default = "UpdateVisitorCount"
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "UpdateVisitorCount"
 }
 
 variable "dynamodb_table_name" {
-  default = "VisitorCount"
+  description = "Name of the DynamoDB table"
+  type        = string
+  default     = "VisitorCount"
 }
 
 variable "lambda_runtime" {
-  default = "python3.12"
+  description = "Lambda runtime version"
+  type        = string
+  default     = "python3.12"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
