a5f5f594c1000fafe429dd2759ac28804bed165f
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-20 13:14:08 +0100

    Update providers.tf

--- monitoring/providers.tf
+++ monitoring/providers.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.5.7"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 5.0"
     }
   }
 }
