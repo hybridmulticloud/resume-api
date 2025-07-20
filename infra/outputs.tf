61ac735c0998001618fd7bc315eebd2aca5c6c9c
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-20 13:33:56 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -27,3 +27,8 @@ output "lambda_execution_role_arn" {
   description = "IAM role ARN assigned to the Lambda function"
   value       = aws_iam_role.lambda_exec.arn
 }
+
+output "lambda_exec_role_name" {
+  description = "Name of the Lambda execution role"
+  value       = aws_iam_role.lambda_exec.name
+}
