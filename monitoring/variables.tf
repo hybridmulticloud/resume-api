d421deabd37d6a3c1f4c0b543d918c32ae624734
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-20 02:27:33 +0100

    Update variables.tf

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -4,7 +4,27 @@ variable "project_name" {
   default     = "resume-api"
 }
 
+variable "aws_region" {
+  description = "AWS region to deploy monitoring resources in"
+  type        = string
+}
+
 variable "alert_email_address" {
-  description = "Email address to receive CloudWatch alarm notifications"
+  description = "Email address to notify on alarm"
+  type        = string
+}
+
+variable "lambda_exec_role_name" {
+  description = "Name of the IAM Role used by the Lambda function"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function to monitor"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table to monitor"
   type        = string
 }
