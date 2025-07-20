84c20004c257c3d1ee9e4565ba3db5362af9aae2
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-20 02:59:11 +0100

    Update variables.tf

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -1,10 +1,13 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
 variable "project_name" {
-  description = "Base name for all resources"
-  type        = string
-  default     = "resume-api"
+  type    = string
+  default = "resume-api"
 }
 
 variable "alert_email_address" {
-  description = "Email address to notify on alarm"
-  type        = string
+  type = string
 }
