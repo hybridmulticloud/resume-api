65cc891c31bc4abb1d34fc0bdeb8721951327ba4
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-20 02:57:03 +0100

    Update variables.tf

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -13,18 +13,3 @@ variable "alert_email_address" {
   description = "Email address to notify on alarm"
   type        = string
 }
-
-variable "lambda_exec_role_name" {
-  description = "Name of the IAM Role used by the Lambda function"
-  type        = string
-}
-
-variable "lambda_function_name" {
-  description = "Name of the Lambda function to monitor"
-  type        = string
-}
-
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table to monitor"
-  type        = string
-}
