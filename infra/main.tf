dbadc5ec9750072bbc28444e3788ae88a8fb4415
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-21 13:59:49 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,11 +8,6 @@ resource "aws_s3_bucket" "lambda_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "lambda_acl" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "lambda_zip" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
