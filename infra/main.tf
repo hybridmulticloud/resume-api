9184504aed98f9132f44d8ee6ed0637d532be215
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-21 13:51:41 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -95,13 +95,6 @@ resource "aws_apigatewayv2_api" "lambda_api" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.lambda_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.update_visitor_count.invoke_arn
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
 
 resource "aws_apigatewayv2_route" "lambda_route" {
   api_id    = aws_apigatewayv2_api.lambda_api.id
@@ -118,12 +111,4 @@ resource "aws_apigatewayv2_stage" "default" {
     throttling_burst_limit = 5000
     throttling_rate_limit  = 10000
   }
-}
-
-resource "aws_lambda_permission" "apigw_invoke" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.update_visitor_count.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
