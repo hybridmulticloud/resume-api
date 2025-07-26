736a7856c92b1f2836edb1c31faf7489e8860715
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 11:17:10 +0100

    Update canary-api.js

--- monitoring/canary-api.js
+++ monitoring/canary-api.js
@@ -1,10 +1,25 @@
-const synthetics = require('Synthetics');
-const log = require('SyntheticsLogger');
-const url = "https://${aws_api_gateway_rest_api.main.execution_arn}/${var.api_stage_name}/UpdateVisitorCount";
-const res = await synthetics.executeHttpStep('post-count', {
-  url,
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({})
-});
-if (res.statusCode !== 200) throw new Error(`Status ${res.statusCode}`);
+resource "aws_synthetics_canary" "api" {
+  name                      = "${var.rest_api_id}-api-canary"
+  artifact_s3_location      = "s3://${var.canary_artifact_bucket}/api/"
+  execution_role_arn        = var.canary_execution_role_arn
+  runtime_version           = "syn-nodejs-4.0"
+  start_time                = null
+
+  schedule {
+    expression = "rate(5 minutes)"
+  }
+
+  run_config {
+    timeout_in_seconds = 60
+  }
+
+  source_code {
+    s3_bucket_arn = null
+    handler       = "index.handler"
+    script        = file("${path.module}/canary-api.js")
+  }
+
+  tags = {
+    Name = "API Canary"
+  }
+}
