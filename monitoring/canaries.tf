0a590eeb579fcaa6fe9fd758810174db67764617
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 12:59:24 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -1,64 +1,60 @@
- locals {
-   homepage_dir = "${path.module}/canaries/homepage"
-   api_dir      = "${path.module}/canaries/api"
- }
+locals {
+  homepage_dir = "${path.module}/canaries/homepage"
+  api_dir      = "${path.module}/canaries/api"
+}
 
- data "archive_file" "homepage_zip" {
-   type        = "zip"
-   source_dir  = local.homepage_dir
--  output_base64 = true
-+  output_path = "${path.module}/homepage.zip"
- }
+data "archive_file" "homepage_zip" {
+  type        = "zip"
+  source_dir  = local.homepage_dir
+  output_path = "${path.module}/homepage.zip"
+}
 
- data "archive_file" "api_zip" {
-   type        = "zip"
-   source_dir  = local.api_dir
--  output_base64 = true
-+  output_path = "${path.module}/api.zip"
- }
+data "archive_file" "api_zip" {
+  type        = "zip"
+  source_dir  = local.api_dir
+  output_path = "${path.module}/api.zip"
+}
 
- resource "aws_synthetics_canary" "homepage" {
-   name               = var.homepage_canary_name
-   execution_role_arn = var.canary_execution_role_arn
-   runtime_version    = "syn-nodejs-puppeteer-3.6"
+resource "aws_synthetics_canary" "homepage" {
+  name               = var.homepage_canary_name
+  execution_role_arn = var.canary_execution_role_arn
+  runtime_version    = "syn-nodejs-puppeteer-3.6"
 
-   artifact_config {
-     s3_location = aws_s3_bucket.canary_artifacts.bucket
-   }
+  artifact_config {
+    s3_location = aws_s3_bucket.canary_artifacts.bucket
+  }
 
-   schedule {
-     expression          = var.schedule_expression
-     duration_in_seconds = 120
-   }
+  schedule {
+    expression          = var.schedule_expression
+    duration_in_seconds = 120
+  }
 
-   start_canary = true
--  zip_file     = data.archive_file.homepage_zip.output_base64
-+  zip_file     = filebase64(data.archive_file.homepage_zip.output_path)
+  start_canary = true
+  zip_file     = filebase64(data.archive_file.homepage_zip.output_path)
 
-   tags = {
-     Component = "HomepageCanary"
-   }
- }
+  tags = {
+    Component = "HomepageCanary"
+  }
+}
 
- resource "aws_synthetics_canary" "api" {
-   name               = var.api_canary_name
-   execution_role_arn = var.canary_execution_role_arn
-   runtime_version    = "syn-nodejs-puppeteer-3.6"
+resource "aws_synthetics_canary" "api" {
+  name               = var.api_canary_name
+  execution_role_arn = var.canary_execution_role_arn
+  runtime_version    = "syn-nodejs-puppeteer-3.6"
 
-   artifact_config {
-     s3_location = aws_s3_bucket.canary_artifacts.bucket
-   }
+  artifact_config {
+    s3_location = aws_s3_bucket.canary_artifacts.bucket
+  }
 
-   schedule {
-     expression          = var.schedule_expression
-     duration_in_seconds = 120
-   }
+  schedule {
+    expression          = var.schedule_expression
+    duration_in_seconds = 120
+  }
 
-   start_canary = false
--  zip_file     = data.archive_file.api_zip.output_base64
-+  zip_file     = filebase64(data.archive_file.api_zip.output_path)
+  start_canary = false
+  zip_file     = filebase64(data.archive_file.api_zip.output_path)
 
-   tags = {
-     Component = "ApiCanary"
-   }
- }
+  tags = {
+    Component = "ApiCanary"
+  }
+}
