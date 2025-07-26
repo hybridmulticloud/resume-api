25fef1ef9cff3b458b5524b367c817be88eaaf17
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 18:26:08 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -1,7 +1,23 @@
+# Generate a random suffix so buckets are globally unique
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
+# Build the assume-role policy for Synthetics Canaries
+data "aws_iam_policy_document" "canary_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["synthetics.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# IAM role for your Synthetics Canary
 resource "aws_iam_role" "canary_role" {
   name               = "${var.project_name}-synthetics-role"
   assume_role_policy = data.aws_iam_policy_document.canary_assume.json
@@ -12,6 +28,19 @@ resource "aws_iam_role" "canary_role" {
   }
 }
 
+# Attach the AWSLambdaBasicExecutionRole managed policy
+resource "aws_iam_role_policy_attachment" "basic" {
+  role       = aws_iam_role.canary_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Attach the Synthetics full-access policy
+resource "aws_iam_role_policy_attachment" "synthetics" {
+  role       = aws_iam_role.canary_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess"
+}
+
+# S3 bucket for raw Canary artifacts
 resource "aws_s3_bucket" "artifacts" {
   bucket        = "${var.project_name}-canary-artifacts"
   force_destroy = true
