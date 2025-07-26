734b50c0bb8a6c7e7dde1f57d1c6affe51acfd44
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 21:22:20 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -2,24 +2,31 @@ terraform {
   required_version = ">= 1.2"
 }
 
-# Point at the infra state file (no S3 or remote bucket required)
 data "terraform_remote_state" "infra" {
   backend = "local"
-
   config = {
     path = "../infra/terraform.tfstate"
   }
 }
 
-# Convenience locals for each infra output.  
-# Update each key to the exact name you declared in infra/outputs.tf.
 locals {
-  api_endpoint       = data.terraform_remote_state.infra.outputs.api_endpoint
-  api_url             = data.terraform_remote_state.infra.outputs.api_gateway_url
-  api_gateway_id   = split(".", replace(local.api_endpoint, "https://", ""))[0]
-  lambda_function_arn = data.terraform_remote_state.infra.outputs.lambda_function_arn
-  lambda_function_name  = var.lambda_function_name
-  
-  dynamodb_table_name = data.terraform_remote_state.infra.outputs.dynamodb_table_name
-  website_bucket_name = data.terraform_remote_state.infra.outputs.website_bucket_name
+  # Lambda outputs
+  lambda_bucket_name        = data.terraform_remote_state.infra.outputs.lambda_bucket_name
+  lambda_function_name      = data.terraform_remote_state.infra.outputs.lambda_function_name
+  lambda_execution_role_arn = data.terraform_remote_state.infra.outputs.lambda_execution_role_arn
+  lambda_exec_role_name     = data.terraform_remote_state.infra.outputs.lambda_exec_role_name
+
+  # API Gateway
+  api_endpoint   = data.terraform_remote_state.infra.outputs.api_endpoint
+  api_gateway_url = data.terraform_remote_state.infra.outputs.api_gateway_url
+  api_gateway_id = split(".", replace(local.api_gateway_url, "https://", ""))[0]
+
+  # DynamoDB & Frontend bucket
+  dynamodb_table_name  = data.terraform_remote_state.infra.outputs.dynamodb_table_name
+  frontend_bucket_name = data.terraform_remote_state.infra.outputs.frontend_bucket_name
+
+  # CloudFront / Route53
+  cloudfront_distribution_id = data.terraform_remote_state.infra.outputs.cloudfront_distribution_id
+  cloudfront_oac_id          = data.terraform_remote_state.infra.outputs.cloudfront_oac_id
+  route53_zone_id            = data.terraform_remote_state.infra.outputs.route53_zone_id
 }
