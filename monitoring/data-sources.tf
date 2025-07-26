11bb3a0b842d0ecf1013e1a47030af2f46c5c4bb
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 14:22:00 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,15 +1,26 @@
+# 1) Look up the REST API by the same base name
 data "aws_api_gateway_rest_api" "api" {
-  name = var.rest_api_name
+  name = var.project_name
 }
 
+# 2) Look up the Lambda function by name
 data "aws_lambda_function" "api_fn" {
   function_name = var.lambda_function_name
 }
 
-data "aws_cloudfront_distribution" "spa" {
-  id = var.cloudfront_distribution_id
+# 3) Discover the CloudFront distribution via your CNAME
+data "aws_cloudfront_distributions" "spa_list" {
+  filter {
+    name   = "CNAME"
+    values = [var.frontend_domain]
+  }
 }
 
+data "aws_cloudfront_distribution" "spa" {
+  id = data.aws_cloudfront_distributions.spa_list.items[0].id
+}
+
+# 4) Lookup the S3 bucket that hosts your SPA
 data "aws_s3_bucket" "site" {
-  bucket = var.site_bucket_name
+  bucket = var.frontend_domain
 }
