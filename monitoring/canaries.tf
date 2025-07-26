cc09b35fa395a05b93d101686e139ebc1341b077
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 14:47:37 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -1,4 +1,5 @@
-resource "aws_iam_role" "canary_exec_role" {
+# Execution role for both canaries
+resource "aws_iam_role" "canary_role" {
   name = "${var.project_name}-synthetics-role"
 
   assume_role_policy = jsonencode({
@@ -11,50 +12,75 @@ resource "aws_iam_role" "canary_exec_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "canary_logs" {
-  role       = aws_iam_role.canary_exec_role.name
+resource "aws_iam_role_policy_attachment" "basic" {
+  role       = aws_iam_role.canary_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "canary_synthetics" {
-  role       = aws_iam_role.canary_exec_role.name
+resource "aws_iam_role_policy_attachment" "synthetics" {
+  role       = aws_iam_role.canary_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess"
 }
 
-# ---- Homepage Canary ----
-resource "aws_synthetics_canary" "homepage" {
-  name               = var.homepage_canary_name
-  execution_role_arn = aws_iam_role.canary_exec_role.arn
-  runtime_version    = "syn-python-selenium-1.0"
-  schedule {
-    expression = var.schedule_expression
-  }
-  artifact_s3_location = "s3://${data.aws_s3_bucket.site.bucket}/canary-artifacts/${var.homepage_canary_name}"
-  handler               = "pageLoadBlueprint.handler"
+# Bucket for canary artifacts
+resource "aws_s3_bucket" "artifacts" {
+  bucket        = "${var.project_name}-canary-artifacts"
+  acl           = "private"
+  force_destroy = true
 
-  code {
-    handler = "pageLoadBlueprint.handler"
-    bucket  = data.aws_s3_bucket.site.bucket
-    key     = "canary-scripts/homepage.zip"
+  versioning {
+    enabled = true
   }
 }
 
-# ---- API Canary ----
-resource "aws_synthetics_canary" "api" {
-  name               = var.api_canary_name
-  execution_role_arn = aws_iam_role.canary_exec_role.arn
-  runtime_version    = "syn-nodejs-puppeteer-3.2"
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Homepage canary (you drop homepage.zip into canaries/homepage/)
+data "archive_file" "homepage" {
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/homepage"
+  output_path = "${path.module}/homepage.zip"
+}
+
+resource "aws_synthetics_canary" "homepage" {
+  name                 = var.homepage_canary_name
+  execution_role_arn   = aws_iam_role.canary_role.arn
+  runtime_version      = "syn-python-selenium-1.0"
+  handler              = "pageLoadBlueprint.handler"
   schedule {
     expression = var.schedule_expression
   }
-  artifact_s3_location = "s3://${data.aws_s3_bucket.site.bucket}/canary-artifacts/${var.api_canary_name}"
-  handler               = "apiTest.handler"
 
-  code {
-    handler = "apiTest.handler"
-    bucket  = data.aws_s3_bucket.site.bucket
-    key     = "canary-scripts/api.zip"
+  artifact_s3_location = "s3://${aws_s3_bucket.artifacts.bucket}/${var.homepage_canary_name}"
+
+  zip_file = filebase64(data.archive_file.homepage.output_path)
+}
+
+# API canary (you drop api.zip into canaries/api/)
+data "archive_file" "api" {
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/api"
+  output_path = "${path.module}/api.zip"
+}
+
+resource "aws_synthetics_canary" "api" {
+  name                 = var.api_canary_name
+  execution_role_arn   = aws_iam_role.canary_role.arn
+  runtime_version      = "syn-nodejs-puppeteer-3.6"
+  handler              = "index.handler"
+  schedule {
+    expression = var.schedule_expression
   }
 
-  depends_on = [aws_iam_role_policy_attachment.canary_synthetics]
+  artifact_s3_location = "s3://${aws_s3_bucket.artifacts.bucket}/${var.api_canary_name}"
+
+  zip_file = filebase64(data.archive_file.api.output_path)
 }
