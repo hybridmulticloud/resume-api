c9359d9c2524e03099d0523fcfe4c28584aff051
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 11:18:05 +0100

    Update canary-homepage.js

--- monitoring/canary-homepage.js
+++ monitoring/canary-homepage.js
@@ -1,9 +1,8 @@
 resource "aws_synthetics_canary" "homepage" {
-  name                      = "${var.rest_api_id}-homepage-canary"
-  artifact_s3_location      = "s3://${var.canary_artifact_bucket}/homepage/"
-  execution_role_arn        = var.canary_execution_role_arn
-  runtime_version           = "syn-nodejs-4.0"
-  start_time                = null
+  name                   = "${var.rest_api_id}-homepage-canary"
+  artifact_s3_location   = "s3://${var.canary_artifact_bucket}/homepage/"
+  execution_role_arn     = var.canary_execution_role_arn
+  runtime_version        = "syn-nodejs-4.0"
 
   schedule {
     expression = "rate(5 minutes)"
@@ -14,9 +13,8 @@ resource "aws_synthetics_canary" "homepage" {
   }
 
   source_code {
-    s3_bucket_arn = null
-    handler       = "index.handler"
-    script        = file("${path.module}/canary-homepage.js")
+    handler = "index.handler"
+    script  = file("${path.module}/canary-homepage.js")
   }
 
   tags = {
