5bd6258188fd5602d0672fd10a860b219a4ed3b9
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 17:48:15 +0100

    Update outputs.tf

--- monitoring/outputs.tf
+++ monitoring/outputs.tf
@@ -1,26 +1,14 @@
+output "api_gateway_id" {
+  value       = local.api_id
+  description = "Extracted API Gateway ID"
+}
+
 output "sns_topic_arn" {
-  value = aws_sns_topic.alerts.arn
-}
-
-output "homepage_canary_name" {
-  value = aws_synthetics_canary.homepage.name
-}
-
-output "api_canary_name" {
-  value = aws_synthetics_canary.api.name
+  value       = aws_sns_topic.alerts.arn
+  description = "SNS topic ARN for alerts"
 }
 
 output "dashboard_name" {
-  value = aws_cloudwatch_dashboard.main.dashboard_name
-}
-
-# Now outputs pull from the local.api_id instead of a (non-existent) data source
-output "api_gateway_id" {
-  description = "The ID of the API Gateway identified by name"
-  value       = local.api_id
-}
-
-output "monitored_api_id" {
-  description = "The ID of the monitored API Gateway"
-  value       = local.api_id
+  value       = aws_cloudwatch_dashboard.main.dashboard_name
+  description = "Name of the CloudWatch dashboard"
 }
