3fe648762cb34a59d1f4c8925973eea38559c5ca
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 11:18:19 +0100

    Update canary-api.js

--- monitoring/canary-api.js
+++ monitoring/canary-api.js
@@ -1,9 +1,8 @@
 resource "aws_synthetics_canary" "api" {
-  name                      = "${var.rest_api_id}-api-canary"
-  artifact_s3_location      = "s3://${var.canary_artifact_bucket}/api/"
-  execution_role_arn        = var.canary_execution_role_arn
-  runtime_version           = "syn-nodejs-4.0"
-  start_time                = null
+  name                   = "${var.rest_api_id}-api-canary"
+  artifact_s3_location   = "s3://${var.canary_artifact_bucket}/api/"
+  execution_role_arn     = var.canary_execution_role_arn
+  runtime_version        = "syn-nodejs-4.0"
 
   schedule {
     expression = "rate(5 minutes)"
@@ -14,9 +13,8 @@ resource "aws_synthetics_canary" "api" {
   }
 
   source_code {
-    s3_bucket_arn = null
-    handler       = "index.handler"
-    script        = file("${path.module}/canary-api.js")
+    handler = "index.handler"
+    script  = file("${path.module}/canary-api.js")
   }
 
   tags = {
