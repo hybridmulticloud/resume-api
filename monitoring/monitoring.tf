c29b80e022c55ccc7faa2b4284f5af0718f3f4af
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 17:47:59 +0100

    Update monitoring.tf

--- monitoring/monitoring.tf
+++ monitoring/monitoring.tf
@@ -1,81 +1,44 @@
-# SNS topic for alerts
 resource "aws_sns_topic" "alerts" {
   name = "${var.project_name}-alerts"
+
+  timeouts {
+    create = "1m"
+    delete = "1m"
+  }
 }
 
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.alerts.arn
-  protocol  = "email"
-  endpoint  = var.alert_email
-}
-
-# API Gateway v2 5XX errors
 resource "aws_cloudwatch_metric_alarm" "api_5xx" {
   alarm_name          = "${var.project_name}-api-5xx"
   namespace           = "AWS/ApiGateway"
   metric_name         = "5XXError"
-  dimensions = {
-    ApiId = local.api_id
-  }
-  statistic           = "Sum"
+  dimensions          = { ApiId = local.api_id }
   period              = 300
   evaluation_periods  = 1
   threshold           = 1
   comparison_operator = "GreaterThanOrEqualToThreshold"
   alarm_actions       = [aws_sns_topic.alerts.arn]
-  ok_actions          = [aws_sns_topic.alerts.arn]
 }
 
-# Lambda function Errors
-resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
-  alarm_name          = "${var.lambda_function_name}-errors"
-  namespace           = "AWS/Lambda"
-  metric_name         = "Errors"
-  dimensions = {
-    FunctionName = var.lambda_function_name
-  }
-  statistic           = "Sum"
-  period              = 300
-  evaluation_periods  = 1
-  threshold           = 1
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  alarm_actions       = [aws_sns_topic.alerts.arn]
-  ok_actions          = [aws_sns_topic.alerts.arn]
-}
-
-# Combined CloudWatch Dashboard
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = "${var.project_name}-dashboard"
-  dashboard_body = jsonencode({
-    widgets = [
-      {
-        type       = "metric"
-        x          = 0
-        y          = 0
-        width      = 12
-        height     = 6
-        properties = {
-          metrics = [[ "AWS/ApiGateway", "5XXError", "ApiId", local.api_id ]]
-          region  = var.aws_region
-          title   = "API Gateway 5XX Errors"
-          stat    = "Sum"
-          period  = 300
-        }
-      },
-      {
-        type       = "metric"
-        x          = 12
-        y          = 0
-        width      = 12
-        height     = 6
-        properties = {
-          metrics = [[ "AWS/Lambda", "Errors", "FunctionName", var.lambda_function_name ]]
-          region  = var.aws_region
-          title   = "Lambda Function Errors"
-          stat    = "Sum"
-          period  = 300
-        }
+  dashboard_body = jsonencode({ widgets = [
+    { type = "metric", x = 0, y = 0, width = 12, height = 6,
+      properties = {
+        metrics = [[ "AWS/ApiGateway", "5XXError", "ApiId", local.api_id ]]
+        stat    = "Sum"
+        period  = 300
+        region  = var.aws_region
+        title   = "API Gateway 5XX Errors"
       }
-    ]
-  })
+    },
+    { type = "metric", x = 12, y = 0, width = 12, height = 6,
+      properties = {
+        metrics = [[ "AWS/Lambda", "Errors", "FunctionName", var.lambda_function_name ]]
+        stat    = "Sum"
+        period  = 300
+        region  = var.aws_region
+        title   = "Lambda Function Errors"
+      }
+    }
+  ]})
 }
