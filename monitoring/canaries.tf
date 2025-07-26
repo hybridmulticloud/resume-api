d60cbc024be194f6d219d4c3aa8c1d3d0f478aa3
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 20:46:24 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -10,28 +10,6 @@ data "aws_iam_policy_document" "canary_assume" {
   }
 }
 
-// existing role (will be imported)
-resource "aws_iam_role" "canary_role" {
-  name               = "resume-api-synthetics-role"
-  assume_role_policy = data.aws_iam_policy_document.canary_assume.json
-
-  lifecycle {
-    prevent_destroy       = true
-    create_before_destroy = false
-    ignore_changes        = [assume_role_policy]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "basic" {
-  role       = aws_iam_role.canary_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "synthetics" {
-  role       = aws_iam_role.canary_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess"
-}
-
 // zip API folder
 data "archive_file" "api_canary" {
   type        = "zip"
