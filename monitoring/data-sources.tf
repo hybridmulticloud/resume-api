d8234241a44a21ec24bff6fa72c699088dc249ec
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 14:50:30 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,9 +1,26 @@
-# Lookup HTTP API by project_name
-data "aws_apigatewayv2_api" "api" {
-  name = var.project_name
+// data-sources.tf
+
+variable "project_name" {
+  type    = string
+  default = "resume-api"
 }
 
-# Lookup Lambda function by name
+variable "lambda_function_name" {
+  type    = string
+  default = "UpdateVisitorCount"
+}
+
+// 1. List all HTTP APIs, filter by name = your project_name
+data "aws_apigatewayv2_apis" "by_name" {
+  names = [var.project_name]
+}
+
+// 2. Pull the first (and only) match into a local
+locals {
+  api_id = data.aws_apigatewayv2_apis.by_name.ids[0]
+}
+
+// 3. Lookup your Lambda by its existing name
 data "aws_lambda_function" "api_fn" {
   function_name = var.lambda_function_name
 }
