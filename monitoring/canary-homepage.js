9cb0ed3d012e0b8dab2bdd142cd9983f68d80a7a
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 11:16:57 +0100

    Update canary-homepage.js

--- monitoring/canary-homepage.js
+++ monitoring/canary-homepage.js
@@ -1,4 +1,25 @@
-const synthetics = require('Synthetics');
-const page = await synthetics.getPage();
-const res = await page.goto("https://${aws_cloudfront_distribution.main.domain_name}", { waitUntil: 'networkidle0' });
-if (res.status() !== 200) throw new Error(`Status ${res.status()}`);
+resource "aws_synthetics_canary" "homepage" {
+  name                      = "${var.rest_api_id}-homepage-canary"
+  artifact_s3_location      = "s3://${var.canary_artifact_bucket}/homepage/"
+  execution_role_arn        = var.canary_execution_role_arn
+  runtime_version           = "syn-nodejs-4.0"
+  start_time                = null
+
+  schedule {
+    expression = "rate(5 minutes)"
+  }
+
+  run_config {
+    timeout_in_seconds = 60
+  }
+
+  source_code {
+    s3_bucket_arn = null
+    handler       = "index.handler"
+    script        = file("${path.module}/canary-homepage.js")
+  }
+
+  tags = {
+    Name = "Homepage Canary"
+  }
+}
