b967660267f848e8601ec17236598d4436c8d98c
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 15:28:40 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,3 +1,10 @@
-data "aws_apigatewayv2_api" "monitored_api" {
-  name = var.api_gateway_name
+# Fetch the list of HTTP/WebSocket APIs by name,
+# then pick the first ID into a local for easy reuse.
+data "aws_apigatewayv2_apis" "monitored_api" {
+  name          = var.api_gateway_name
+  protocol_type = var.api_gateway_protocol_type
+}
+
+locals {
+  api_id = tolist(data.aws_apigatewayv2_apis.monitored_api.ids)[0]
 }
