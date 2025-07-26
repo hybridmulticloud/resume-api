d8ed7524b2bda234048a94c057f883362b7b235c
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 13:33:56 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -16,21 +16,23 @@ data "archive_file" "api_zip" {
 }
 
 resource "aws_synthetics_canary" "homepage" {
-  name               = var.homepage_canary_name
-  execution_role_arn = var.canary_execution_role_arn
-  runtime_version    = "syn-nodejs-puppeteer-3.6"
-
-  artifact_config {
-    s3_location = aws_s3_bucket.canary_artifacts.bucket
-  }
+  name                 = var.homepage_canary_name
+  artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/homepage/"
+  execution_role_arn   = var.canary_execution_role_arn
+  runtime_version      = "syn-nodejs-puppeteer-3.6"
+  handler              = "index.handler"
+  start_canary         = true
 
   schedule {
     expression          = var.schedule_expression
     duration_in_seconds = 120
   }
 
-  start_canary = true
-  zip_file     = filebase64(data.archive_file.homepage_zip.output_path)
+  run_config {
+    timeout_in_seconds = 60
+  }
+
+  zip_file = filebase64(data.archive_file.homepage_zip.output_path)
 
   tags = {
     Component = "HomepageCanary"
@@ -38,21 +40,23 @@ resource "aws_synthetics_canary" "homepage" {
 }
 
 resource "aws_synthetics_canary" "api" {
-  name               = var.api_canary_name
-  execution_role_arn = var.canary_execution_role_arn
-  runtime_version    = "syn-nodejs-puppeteer-3.6"
-
-  artifact_config {
-    s3_location = aws_s3_bucket.canary_artifacts.bucket
-  }
+  name                 = var.api_canary_name
+  artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/api/"
+  execution_role_arn   = var.canary_execution_role_arn
+  runtime_version      = "syn-nodejs-puppeteer-3.6"
+  handler              = "index.handler"
+  start_canary         = false
 
   schedule {
     expression          = var.schedule_expression
     duration_in_seconds = 120
   }
 
-  start_canary = false
-  zip_file     = filebase64(data.archive_file.api_zip.output_path)
+  run_config {
+    timeout_in_seconds = 60
+  }
+
+  zip_file = filebase64(data.archive_file.api_zip.output_path)
 
   tags = {
     Component = "ApiCanary"
