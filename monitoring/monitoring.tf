cbf25f10eb2bc9c55b943403f0900a6ff89a84c3
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 11:29:28 +0100

    Update monitoring.tf

--- monitoring/monitoring.tf
+++ monitoring/monitoring.tf
@@ -56,7 +56,12 @@ resource "aws_synthetics_canary" "homepage" {
   }
 
   code {
-    script = file("${path.module}/canary-homepage.js")
+    script = <<-EOF
+      const synthetics = require('Synthetics');
+      const page = await synthetics.getPage();
+      const res = await page.goto("https://$${aws_cloudfront_distribution.main.domain_name}", { waitUntil: 'networkidle0' });
+      if (res.status() !== 200) throw new Error(\`Status $${res.status()}\`);
+    EOF
   }
 
   tags = {
@@ -80,7 +85,18 @@ resource "aws_synthetics_canary" "api" {
   }
 
   code {
-    script = file("${path.module}/canary-api.js")
+    script = <<-EOF
+      const synthetics = require('Synthetics');
+      const log = require('SyntheticsLogger');
+      const url = "https://$${aws_api_gateway_rest_api.main.execution_arn}/$${var.api_stage_name}/UpdateVisitorCount";
+      const res = await synthetics.executeHttpStep('post-count', {
+        url,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+      });
+      if (res.statusCode !== 200) throw new Error(\`Status $${res.statusCode}\`);
+    EOF
   }
 
   tags = {
