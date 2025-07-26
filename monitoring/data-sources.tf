93499aeae95513ca8044e752f3199233e933bf48
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 15:14:54 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,14 +1,3 @@
-data "aws_apigatewayv2_apis" "by_name" {
-  filter {
-    name   = "Name"
-    values = [var.project_name]
-  }
-}
-
-locals {
-  api_id = data.aws_apigatewayv2_apis.by_name.ids[0]
-}
-
-data "aws_lambda_function" "api_fn" {
-  function_name = var.lambda_function_name
+data "aws_apigatewayv2_api" "main" {
+  name = var.api_gateway_name
 }
