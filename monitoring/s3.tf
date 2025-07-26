c00bdcc071bffb400e06ba6815a5ca948a9a1705
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 12:37:53 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,4 +1,3 @@
-# Create an S3 bucket for storing canary artifacts (screenshots/logs)
 resource "random_id" "suffix" {
   byte_length = 4
 }
@@ -7,6 +6,18 @@ resource "aws_s3_bucket" "canary_artifacts" {
   bucket        = "canary-artifacts-${random_id.suffix.hex}"
   acl           = "private"
   force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = {
     Name = "CanaryArtifacts"
