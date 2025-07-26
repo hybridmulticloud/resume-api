5d28f5c157f89816a194a4977c1782602461dde9
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 17:13:03 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,19 +1,17 @@
-# Generate a random suffix so buckets are globally unique
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# S3 bucket for Canary artifacts
 resource "aws_s3_bucket" "canary_artifacts" {
   bucket        = "canary-artifacts-${random_id.suffix.hex}"
   force_destroy = true
-
-  tags = {
-    Name = "CanaryArtifacts"
-  }
 }
 
-# Separate versioning resource to avoid deprecation warning
+resource "aws_s3_bucket_acl" "canary_artifacts_acl" {
+  bucket = aws_s3_bucket.canary_artifacts.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_versioning" "canary_artifacts_versioning" {
   bucket = aws_s3_bucket.canary_artifacts.id
 
@@ -22,7 +20,6 @@ resource "aws_s3_bucket_versioning" "canary_artifacts_versioning" {
   }
 }
 
-# Ensure server‐side encryption
 resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts_sse" {
   bucket = aws_s3_bucket.canary_artifacts.id
 
