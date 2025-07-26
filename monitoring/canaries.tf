c69503b5776709282e8abf75f3f7e0d5bcb7a15b
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 14:51:41 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -27,9 +27,13 @@ resource "aws_s3_bucket" "artifacts" {
   bucket        = "${var.project_name}-canary-artifacts"
   acl           = "private"
   force_destroy = true
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
