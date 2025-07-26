4e51e19421c089609ee154364773114b52fd1dfd
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 12:25:56 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,12 +1,24 @@
-data "terraform_remote_state" "backend" {
-  backend = "remote"
-
-  config = {
-    organization = "hybridmulticloud"
-    workspaces   = { name = "resume-api-backend" }
-  }
+# API Gateway
+data "aws_api_gateway_rest_api" "api" {
+  name = var.rest_api_name
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_api_gateway_stage" "api_stage" {
+  rest_api_id = data.aws_api_gateway_rest_api.api.id
+  stage_name  = var.api_stage_name
+}
 
-data "aws_region" "current" {}
+# Lambda (to pull environment/config if needed)
+data "aws_lambda_function" "api_fn" {
+  function_name = var.lambda_function_name
+}
+
+# CloudFront (SPA)
+data "aws_cloudfront_distribution" "spa" {
+  id = var.cloudfront_distribution_id
+}
+
+# S3 static site (for dashboard metrics)
+data "aws_s3_bucket" "site" {
+  bucket = var.site_bucket_name
+}
