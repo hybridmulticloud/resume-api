a5f892785bd3cd6e1483bb531219d6e380c8cd48
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 19:47:21 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,10 +1,19 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "canary_artifacts" {
   bucket        = "canary-artifacts-${random_id.suffix.hex}"
   force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_versioning" "canary_artifacts_versioning" {
   bucket = aws_s3_bucket.canary_artifacts.id
+
   versioning_configuration {
     status = "Enabled"
   }
@@ -12,6 +21,7 @@ resource "aws_s3_bucket_versioning" "canary_artifacts_versioning" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts_sse" {
   bucket = aws_s3_bucket.canary_artifacts.id
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
