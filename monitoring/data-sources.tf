ed6cf5104840bb75abe9ca306574196ab0a3c6ec
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-26 12:37:41 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -1,4 +1,3 @@
-# API Gateway
 data "aws_api_gateway_rest_api" "api" {
   name = var.rest_api_name
 }
@@ -8,17 +7,14 @@ data "aws_api_gateway_stage" "api_stage" {
   stage_name  = var.api_stage_name
 }
 
-# Lambda (to pull environment/config if needed)
 data "aws_lambda_function" "api_fn" {
   function_name = var.lambda_function_name
 }
 
-# CloudFront (SPA)
 data "aws_cloudfront_distribution" "spa" {
   id = var.cloudfront_distribution_id
 }
 
-# S3 static site (for dashboard metrics)
 data "aws_s3_bucket" "site" {
   bucket = var.site_bucket_name
 }
