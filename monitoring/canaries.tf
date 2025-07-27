f94f792bd3d05d8405ebf3452dc6a417ca0875f3
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 00:53:46 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -1,3 +1,5 @@
+# monitoring/canaries.tf
+
 data "archive_file" "api_canary" {
   type        = "zip"
   source_dir  = "${path.module}/canaries/api"
@@ -14,12 +16,10 @@ resource "aws_synthetics_canary" "api" {
   name                 = var.api_canary_name
   execution_role_arn   = local.canary_role_arn
   runtime_version      = "syn-nodejs-puppeteer-3.6"
+  handler              = "index.handler"
   artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/api"
 
-  code {
-    handler  = "index.handler"
-    zip_file = filebase64(data.archive_file.api_canary.output_path)
-  }
+  zip_file = filebase64("${path.module}/.terraform/cache/api_canary.zip")
 
   schedule {
     expression = var.schedule_expression
@@ -36,12 +36,10 @@ resource "aws_synthetics_canary" "homepage" {
   name                 = var.homepage_canary_name
   execution_role_arn   = local.canary_role_arn
   runtime_version      = "syn-python-selenium-1.0"
+  handler              = "pageLoadBlueprint.handler"
   artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/homepage"
 
-  code {
-    handler  = "pageLoadBlueprint.handler"
-    zip_file = filebase64(data.archive_file.homepage_canary.output_path)
-  }
+  zip_file = filebase64("${path.module}/.terraform/cache/homepage_canary.zip")
 
   schedule {
     expression = var.schedule_expression
