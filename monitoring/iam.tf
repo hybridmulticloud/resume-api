479053c0fed84efd38a740eb54742603e772842d
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-27 23:42:21 +0100

    Update iam.tf

--- monitoring/iam.tf
+++ monitoring/iam.tf
@@ -1,6 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 data "aws_iam_policy_document" "assume_synthetics" {
   statement {
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
+
     principals {
       type        = "Service"
       identifiers = ["synthetics.amazonaws.com"]
@@ -14,14 +29,23 @@ data "external" "canary_role_lookup" {
     <<-EOC
       aws iam get-role --role-name ${var.role_name} \
         --query '{role_name:Role.RoleName, arn:Role.Arn}' \
-        --output json 2>/dev/null || echo '{"role_name": ""}'
+        --output json 2>/dev/null || echo '{"role_name": "", "arn": ""}'
     EOC
   ]
 }
 
+locals {
+  # true if an existing IAM role was found
+  role_exists      = length(data.external.canary_role_lookup.result.role_name) > 0
+
+  # choose existing vs. newly created role
+  canary_role_name = local.role_exists ? data.external.canary_role_lookup.result.role_name : aws_iam_role.canary_role[0].name
+  canary_role_arn  = local.role_exists ? data.external.canary_role_lookup.result.arn      : aws_iam_role.canary_role[0].arn
+}
+
 resource "aws_iam_role" "canary_role" {
-  count              = length(data.external.canary_role_lookup.result.role_name) > 0 ? 0 : 1
-  name               = var.role_name
+  count             = local.role_exists ? 0 : 1
+  name              = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_synthetics.json
 
   lifecycle {
@@ -29,19 +53,10 @@ resource "aws_iam_role" "canary_role" {
   }
 }
 
-locals {
-  canary_role_name = length(aws_iam_role.canary_role) > 0
-    ? aws_iam_role.canary_role[0].name
-    : data.external.canary_role_lookup.result.role_name
-
-  canary_role_arn = length(aws_iam_role.canary_role) > 0
-    ? aws_iam_role.canary_role[0].arn
-    : data.external.canary_role_lookup.result.arn
-}
-
 data "aws_iam_policy_document" "canary_policy" {
   statement {
-    actions = [
+    effect    = "Allow"
+    actions   = [
       "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
