2bc8d19fde931bd9ab340a6183641bd00d4d1b5b
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 00:04:59 +0100

    Update data-sources.tf

--- monitoring/data-sources.tf
+++ monitoring/data-sources.tf
@@ -2,7 +2,9 @@ data "terraform_remote_state" "infra" {
   backend = "remote"
   config = {
     organization = "hybridmulticloud"
-    workspaces = { name = "resume-api-backend" }
+    workspaces = {
+      name = "resume-api-backend"
+    }
   }
 }
 
@@ -11,25 +13,24 @@ locals {
   lambda_function_name       = data.terraform_remote_state.infra.outputs.lambda_function_name
   lambda_execution_role_arn  = data.terraform_remote_state.infra.outputs.lambda_execution_role_arn
   lambda_exec_role_name      = data.terraform_remote_state.infra.outputs.lambda_exec_role_name
-
   api_endpoint               = data.terraform_remote_state.infra.outputs.api_endpoint
   api_gateway_url            = data.terraform_remote_state.infra.outputs.api_gateway_url
   api_gateway_id             = split(".", replace(local.api_gateway_url, "https://", ""))[0]
-
   dynamodb_table_name        = data.terraform_remote_state.infra.outputs.dynamodb_table_name
   frontend_bucket_name       = data.terraform_remote_state.infra.outputs.frontend_bucket_name
-
   cloudfront_distribution_id = data.terraform_remote_state.infra.outputs.cloudfront_distribution_id
   cloudfront_oac_id          = data.terraform_remote_state.infra.outputs.cloudfront_oac_id
   route53_zone_id            = data.terraform_remote_state.infra.outputs.route53_zone_id
 }
 
 data "archive_file" "api_canary" {
-  type       = "zip"
-  source_dir = "${path.module}/canaries/api"
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/api"
+  output_path = "${path.module}/canaries/api.zip"
 }
 
 data "archive_file" "homepage_canary" {
-  type       = "zip"
-  source_dir = "${path.module}/canaries/homepage"
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/homepage"
+  output_path = "${path.module}/canaries/homepage.zip"
 }
