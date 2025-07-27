036d28ee6298f6b6ec6876cd64c11efa1d8123eb
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-27 23:40:56 +0100

    Update iam.tf

--- monitoring/iam.tf
+++ monitoring/iam.tf
@@ -12,20 +12,15 @@ data "external" "canary_role_lookup" {
   program = [
     "bash", "-c",
     <<-EOC
-      aws iam get-role \
-        --role-name ${var.role_name} \
+      aws iam get-role --role-name ${var.role_name} \
         --query '{role_name:Role.RoleName, arn:Role.Arn}' \
-        --output json 2>/dev/null || echo '{\"role_name\": \"\"}'
+        --output json 2>/dev/null || echo '{"role_name": ""}'
     EOC
   ]
 }
 
-locals {
-  role_exists = length(data.external.canary_role_lookup.result.role_name) > 0
-}
-
 resource "aws_iam_role" "canary_role" {
-  count              = local.role_exists ? 0 : 1
+  count              = length(data.external.canary_role_lookup.result.role_name) > 0 ? 0 : 1
   name               = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_synthetics.json
 
@@ -35,13 +30,13 @@ resource "aws_iam_role" "canary_role" {
 }
 
 locals {
-  canary_role_name = local.role_exists
-    ? data.external.canary_role_lookup.result.role_name
-    : aws_iam_role.canary_role[0].name
+  canary_role_name = length(aws_iam_role.canary_role) > 0
+    ? aws_iam_role.canary_role[0].name
+    : data.external.canary_role_lookup.result.role_name
 
-  canary_role_arn = local.role_exists
-    ? data.external.canary_role_lookup.result.arn
-    : aws_iam_role.canary_role[0].arn
+  canary_role_arn = length(aws_iam_role.canary_role) > 0
+    ? aws_iam_role.canary_role[0].arn
+    : data.external.canary_role_lookup.result.arn
 }
 
 data "aws_iam_policy_document" "canary_policy" {
@@ -56,4 +51,9 @@ data "aws_iam_policy_document" "canary_policy" {
     ]
     resources = ["*"]
   }
+}
+
+resource "aws_iam_role_policy" "canary_policy" {
+  role   = local.canary_role_name
+  policy = data.aws_iam_policy_document.canary_policy.json
 }
