de343eb58bb54a9e01798befd0faf2aaef045cc6
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-27 23:22:42 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -11,50 +11,46 @@ data "aws_iam_policy_document" "canary_assume" {
   }
 }
 
-resource "aws_synthetics_canary" "api" {
-  name                        = var.api_canary_name
-  execution_role_arn          = aws_iam_role.canary_role.arn
-  runtime_version             = "syn-nodejs-puppeteer-3.6"
-  handler                     = "index.handler"
-  artifact_s3_location        = "s3://${aws_s3_bucket.canary_artifacts.bucket}/api"
-  start_canary_after_creation = true
+data "archive_file" "api_canary" {
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/api"
+  output_path = "${path.module}/canaries/api.zip"
+}
 
+data "archive_file" "homepage_canary" {
+  type        = "zip"
+  source_dir  = "${path.module}/canaries/homepage"
+  output_path = "${path.module}/canaries/homepage.zip"
+}
+
+resource "aws_synthetics_canary" "api" {
+  name                 = var.api_canary_name
+  execution_role_arn   = aws_iam_role.canary_role.arn
+  runtime_version      = "syn-nodejs-puppeteer-3.6"
+  handler              = "index.handler"
+  artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/api"
+  zip_file             = filebase64(data.archive_file.api_canary.output_path)
   schedule {
     expression = var.schedule_expression
   }
-
-  code {
-    handler = "index.handler"
-    script  = file("${path.module}/canaries/api/index.js")
-  }
-
   lifecycle {
     prevent_destroy = true
   }
-
   tags = var.tags
 }
 
 resource "aws_synthetics_canary" "homepage" {
-  name                        = var.homepage_canary_name
-  execution_role_arn          = aws_iam_role.canary_role.arn
-  runtime_version             = "syn-python-selenium-1.0"
-  handler                     = "pageLoadBlueprint.handler"
-  artifact_s3_location        = "s3://${aws_s3_bucket.canary_artifacts.bucket}/homepage"
-  start_canary_after_creation = true
-
+  name                 = var.homepage_canary_name
+  execution_role_arn   = aws_iam_role.canary_role.arn
+  runtime_version      = "syn-python-selenium-1.0"
+  handler              = "pageLoadBlueprint.handler"
+  artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/homepage"
+  zip_file             = filebase64(data.archive_file.homepage_canary.output_path)
   schedule {
     expression = var.schedule_expression
   }
-
-  code {
-    handler = "pageLoadBlueprint.handler"
-    script  = file("${path.module}/canaries/homepage/index.js")
-  }
-
   lifecycle {
     prevent_destroy = true
   }
-
   tags = var.tags
 }
