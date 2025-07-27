012ec30754b259d6bfcc90234892d5a071718b49
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 00:25:38 +0100

    Update canaries.tf

--- monitoring/canaries.tf
+++ monitoring/canaries.tf
@@ -1,13 +1,11 @@
 data "archive_file" "api_canary" {
-  type        = "zip"
-  source_dir  = "${path.module}/canaries/api"
-  output_path = "${path.module}/canaries/api.zip"
+  type       = "zip"
+  source_dir = "${path.module}/canaries/api"
 }
 
 data "archive_file" "homepage_canary" {
-  type        = "zip"
-  source_dir  = "${path.module}/canaries/homepage"
-  output_path = "${path.module}/canaries/homepage.zip"
+  type       = "zip"
+  source_dir = "${path.module}/canaries/homepage"
 }
 
 resource "aws_synthetics_canary" "api" {
@@ -16,10 +14,7 @@ resource "aws_synthetics_canary" "api" {
   runtime_version      = "syn-nodejs-puppeteer-3.6"
   handler              = "index.handler"
   artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/api"
-
-  zip_file = filebase64(
-    data.archive_file.api_canary.output_path
-  )
+  zip_file             = data.archive_file.api_canary.output_base64
 
   schedule {
     expression = var.schedule_expression
@@ -38,10 +33,7 @@ resource "aws_synthetics_canary" "homepage" {
   runtime_version      = "syn-python-selenium-1.0"
   handler              = "pageLoadBlueprint.handler"
   artifact_s3_location = "s3://${aws_s3_bucket.canary_artifacts.bucket}/homepage"
-
-  zip_file = filebase64(
-    data.archive_file.homepage_canary.output_path
-  )
+  zip_file             = data.archive_file.homepage_canary.output_base64
 
   schedule {
     expression = var.schedule_expression
