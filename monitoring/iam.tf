43131a845150f1ae20e53575f2d57e0e0ac445d6
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:17:30 +0100

    Update iam.tf

--- monitoring/iam.tf
+++ monitoring/iam.tf
@@ -1,22 +1,44 @@
+data "aws_iam_policy_document" "canary_assume" {
+  statement {
+    effect    = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["synthetics.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "canary" {
+  name               = "${local.prefix}-canary-role"
+  assume_role_policy = data.aws_iam_policy_document.canary_assume.json
+  tags               = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "synthetics_core" {
+  role       = aws_iam_role.canary.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess"
+}
+
 data "aws_iam_policy_document" "canary_s3" {
   statement {
     effect    = "Allow"
     actions   = ["s3:ListBucket","s3:GetBucketAcl","s3:GetBucketLocation"]
-    resources = [ local.bucket_arn ]
+    resources = [ aws_s3_bucket.canary_artifacts.arn ]
   }
   statement {
     effect    = "Allow"
     actions   = ["s3:PutObject","s3:GetObject"]
-    resources = [ local.bucket_arn_all ]
+    resources = ["${aws_s3_bucket.canary_artifacts.arn}/*"]
   }
 }
 
-resource "aws_iam_policy" "canary_s3_policy" {
+resource "aws_iam_policy" "canary_s3" {
   name   = "${local.prefix}-canary-s3-policy"
   policy = data.aws_iam_policy_document.canary_s3.json
 }
 
-resource "aws_iam_role_policy_attachment" "canary_s3" {
+resource "aws_iam_role_policy_attachment" "canary_s3_attach" {
   role       = aws_iam_role.canary.name
-  policy_arn = aws_iam_policy.canary_s3_policy.arn
+  policy_arn = aws_iam_policy.canary_s3.arn
 }
