484ef67d59a5b3502c1575233f78c60b948e339e
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:30:50 +0100

    Update variables.tf

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -1,23 +1,23 @@
 variable "project_name" {
-  description = "Base name for all resources"
+  description = "Base prefix for all resources"
   type        = string
   default     = "resume-api"
 }
 
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy into"
   type        = string
   default     = "us-east-1"
 }
 
 variable "schedule_expression" {
-  description = "Cron or rate() expression for canary schedules"
+  description = "CloudWatch schedule (rate() or cron()) for canaries"
   type        = string
   default     = "rate(5 minutes)"
 }
 
 variable "additional_tags" {
-  description = "Extra tags to merge"
+  description = "Extra tags to merge with defaults"
   type        = map(string)
   default     = {}
 }
