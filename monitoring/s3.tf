732012a8f1832f6e07f22270b922a3bcd0322a00
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 01:53:44 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -6,13 +6,15 @@ resource "aws_s3_bucket" "canary_artifacts" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "canary_artifacts_sse" {
+  bucket = aws_s3_bucket.canary_artifacts.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
-
-  tags = var.tags
 }
