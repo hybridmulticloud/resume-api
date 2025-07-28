95a074927c8cb70eaec70a4117a38b38afe35909
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 04:17:53 +0100

    Update locals.tf

--- monitoring/locals.tf
+++ monitoring/locals.tf
@@ -1,17 +1,18 @@
+# Retrieve the AWS account ID for a globally-unique bucket name
 data "aws_caller_identity" "me" {}
 
 locals {
-  # Project prefix and derived names
+  # Base prefix for all resources
   prefix               = var.project_name
+
+  # Globally-unique S3 bucket name
   bucket_name          = "${local.prefix}-${data.aws_caller_identity.me.account_id}-canary-artifacts"
+
+  # Canary names derived from the same prefix
   api_canary_name      = "${local.prefix}-api-canary"
   homepage_canary_name = "${local.prefix}-homepage-canary"
 
-  # ARNs for IAM policies
-  bucket_arn     = "arn:aws:s3:::${local.bucket_name}"
-  bucket_arn_all = "${local.bucket_arn}/*"
-
-  # Tags merged with any extras
+  # Common tags, merge in any extras
   tags = merge(
     {
       Project     = var.project_name
