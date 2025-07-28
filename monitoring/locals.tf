2e437baa13b4ceb7b9ff74cc5912d57a413eb4d7
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:17:02 +0100

    Update locals.tf

--- monitoring/locals.tf
+++ monitoring/locals.tf
@@ -1,17 +1,16 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  prefix           = var.project_name
-  bucket_name      = "${local.prefix}-canary-artifacts"
-  bucket_arn       = "arn:aws:s3:::${local.bucket_name}"
-  bucket_arn_all   = "${local.bucket_arn}/*"
-  tags             = merge(
-                       { Project = var.project_name, Environment = "production" },
-                       var.additional_tags,
-                     )
-  create_bucket    = lookup(
-                       data.external.bucket_exists.result, 
-                       "exists", 
-                       "false"
-                     ) == "false"
+  prefix               = var.project_name
+  bucket_name          = "${local.prefix}-${data.aws_caller_identity.current.account_id}-canary-artifacts"
+  api_canary_name      = "${local.prefix}-api-canary"
+  homepage_canary_name = "${local.prefix}-homepage-canary"
+
+  tags = merge(
+    {
+      Project     = var.project_name
+      Environment = "production"
+    },
+    var.additional_tags
+  )
 }
