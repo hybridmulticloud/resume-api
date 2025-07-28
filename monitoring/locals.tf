a017a9e7f50d6b4686baf67bc0cb93fa717760e3
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:42:34 +0100

    Update locals.tf

--- monitoring/locals.tf
+++ monitoring/locals.tf
@@ -1,19 +1,14 @@
 data "aws_caller_identity" "me" {}
 
 locals {
-  prefix               = var.project_name
-  bucket_name          = "${local.prefix}-${data.aws_caller_identity.me.account_id}-canary-artifacts"
-  bucket_arn           = "arn:aws:s3:::${local.bucket_name}"
-  bucket_arn_all       = "${local.bucket_arn}/*"
+  prefix      = var.project_name
+  bucket_name = "${local.prefix}-${data.aws_caller_identity.me.account_id}-canary-artifacts"
 
   api_canary_name      = "${local.prefix}-api-canary"
   homepage_canary_name = "${local.prefix}-homepage-canary"
 
   tags = merge(
-    {
-      Project     = var.project_name
-      Environment = "production"
-    },
+    { Project = var.project_name, Environment = "production" },
     var.additional_tags
   )
 }
