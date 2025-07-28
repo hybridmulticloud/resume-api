193feff22043e0402cb733fe4ad9333abedc00e4
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:43:01 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,13 +1,11 @@
-# Check if it already exists
 data "external" "bucket_exists" {
-  program = [
-    "bash", "-c", <<-EOF
-      if aws s3api head-bucket --bucket ${local.bucket_name} 2>/dev/null; then
-        echo '{"exists":"true"}'
-      else
-        echo '{"exists":"false"}'
-      fi
-    EOF
+  program = ["bash", "-c", <<-EOF
+    if aws s3api head-bucket --bucket ${local.bucket_name} 2>/dev/null; then
+      echo '{"exists":"true"}'
+    else
+      echo '{"exists":"false"}'
+    fi
+EOF
   ]
 }
 
@@ -19,16 +17,10 @@ resource "aws_s3_bucket" "canary_artifacts" {
   count  = local.create_bucket ? 1 : 0
   bucket = local.bucket_name
   tags   = local.tags
-
-  # ensure correct region
-  create_bucket_configuration {
-    location_constraint = var.aws_region
-  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = local.bucket_name
-
   versioning_configuration {
     status = "Enabled"
   }
@@ -36,7 +28,6 @@ resource "aws_s3_bucket_versioning" "versioning" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
   bucket = local.bucket_name
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
