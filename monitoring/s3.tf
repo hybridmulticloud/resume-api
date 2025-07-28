43738e17d120e68c81e5c2c298e3556e66114264
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 01:57:59 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,17 +1,14 @@
 resource "aws_s3_bucket" "canary_artifacts" {
-  bucket = "${var.project_name}-canary-artifacts"
+  bucket = local.bucket_name
   acl    = "private"
-
   versioning {
     enabled = true
   }
-
-  tags = var.tags
+  tags = local.tags
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "canary_artifacts_sse" {
   bucket = aws_s3_bucket.canary_artifacts.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
