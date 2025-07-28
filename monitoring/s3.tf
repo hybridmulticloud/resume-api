57ec50627f982c4105a43ef5030af85ae9fe612c
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 04:50:28 +0100

    Update s3.tf

--- monitoring/s3.tf
+++ monitoring/s3.tf
@@ -1,17 +1,10 @@
 resource "aws_s3_bucket" "canary_artifacts" {
-  provider = aws.use1   # if you need us-east-1; otherwise omit
-  bucket   = local.bucket_name
-  tags     = local.tags
-
-  # if your provider is already in the right region, you can omit this block
-  create_bucket_configuration {
-    location_constraint = var.aws_region
-  }
+  bucket = local.bucket_name
+  tags   = local.tags
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  provider = aws.use1   # or default aws
-  bucket   = aws_s3_bucket.canary_artifacts.id
+  bucket = aws_s3_bucket.canary_artifacts.id
 
   versioning_configuration {
     status = "Enabled"
@@ -19,8 +12,7 @@ resource "aws_s3_bucket_versioning" "versioning" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  provider = aws.use1   # or default aws
-  bucket   = aws_s3_bucket.canary_artifacts.id
+  bucket = aws_s3_bucket.canary_artifacts.id
 
   rule {
     apply_server_side_encryption_by_default {
