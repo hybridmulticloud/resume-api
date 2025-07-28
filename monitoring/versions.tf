d18599551860a60b446a2dbc4c1a2b71f3b0e38a
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 01:04:17 +0100

    Update versions.tf

--- monitoring/versions.tf
+++ monitoring/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=1.2"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4.0"
+      version = ">= 4.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~>2.3"
+      version = ">= 2.1.0"
     }
   }
 }
