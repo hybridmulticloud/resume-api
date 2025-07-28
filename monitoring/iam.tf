cd7d3f977f6750de9535efce7b7b5531b7fbd088
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-28 02:13:52 +0100

    Update iam.tf

--- monitoring/iam.tf
+++ monitoring/iam.tf
@@ -1,61 +1,22 @@
-data "aws_iam_policy_document" "canary_assume" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["synthetics.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "canary" {
-  name               = "${var.project_name}-canary-role"
-  assume_role_policy = data.aws_iam_policy_document.canary_assume.json
-  tags               = local.tags
-}
-
-resource "aws_iam_role_policy_attachment" "synthetics_core" {
-  role       = aws_iam_role.canary.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchSyntheticsFullAccess"
-}
-
 data "aws_iam_policy_document" "canary_s3" {
   statement {
-    effect = "Allow"
-    actions = [
-      "s3:CreateBucket",
-      "s3:ListAllMyBuckets",
-      "s3:ListBucket",
-      "s3:ListBucketVersions",
-      "s3:GetBucketAcl",
-      "s3:GetBucketCors",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketVersioning",
-      "s3:GetBucketWebsite",
-      "s3:GetEncryptionConfiguration",
-      "s3:PutBucketAcl",
-      "s3:PutBucketCors",
-      "s3:PutBucketPolicy",
-      "s3:PutBucketWebsite",
-      "s3:PutBucketVersioning",
-      "s3:PutEncryptionConfiguration",
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-    resources = [
-      aws_s3_bucket.canary_artifacts.arn,
-      "${aws_s3_bucket.canary_artifacts.arn}/*"
-    ]
+    effect    = "Allow"
+    actions   = ["s3:ListBucket","s3:GetBucketAcl","s3:GetBucketLocation"]
+    resources = [ local.bucket_arn ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject","s3:GetObject"]
+    resources = [ local.bucket_arn_all ]
   }
 }
 
 resource "aws_iam_policy" "canary_s3_policy" {
-  name   = "${var.project_name}-canary-s3"
+  name   = "${local.prefix}-canary-s3-policy"
   policy = data.aws_iam_policy_document.canary_s3.json
 }
 
-resource "aws_iam_role_policy_attachment" "canary_s3_attach" {
+resource "aws_iam_role_policy_attachment" "canary_s3" {
   role       = aws_iam_role.canary.name
   policy_arn = aws_iam_policy.canary_s3_policy.arn
 }
